f1bd1c3516000677c44e269dc9bc1bcada6ed757
Ishaan Verma <63767298+IshaanVerma1130@users.noreply.github.com>
2021-08-06 17:56:46 +0530

    added horizontal movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,9 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("In m/s")] [SerializeField] float xRange = 10f;
+    [Tooltip("In m/s")] [SerializeField] float ySpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        //float yThrow = CrossPlatformInputManager.GetAxis("vertical");
+
+        float xOffset = xRange * xThrow * Time.deltaTime;
+        float xRaw = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(xRaw, -xRange, xRange);
+        //yOffset += ySpeed * yThrow * Time.deltaTime;
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
     }
 }
