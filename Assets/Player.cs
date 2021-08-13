e2cfd3908ef89060c6fa70236410746a780095cf
Ishaan Verma <63767298+IshaanVerma1130@users.noreply.github.com>
2021-08-14 01:03:29 +0530

    added pitch yaw and roll along with vertical movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,26 +5,50 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In m/s")] [SerializeField] float xRange = 10f;
-    [Tooltip("In m/s")] [SerializeField] float ySpeed = 10f;
+	[Tooltip("In m/s")] [SerializeField] float xRange = 7f;
+	[Tooltip("In m/s")] [SerializeField] float yRange = 5f;
+	[SerializeField] float positionPitchFactor = -4.5f;
+	[SerializeField] float rotationPitchFactor = -10f;
+	[SerializeField] float rotationRollFactor = -6f;
+	[SerializeField] float rotationYawFactor = 5f;
+	float xThrow, yThrow;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        //float yThrow = CrossPlatformInputManager.GetAxis("vertical");
+	// Update is called once per frame
+	void Update()
+	{
+		ProcessTranslation();
+		ProcessRotation();
+	}
 
-        float xOffset = xRange * xThrow * Time.deltaTime;
-        float xRaw = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(xRaw, -xRange, xRange);
-        //yOffset += ySpeed * yThrow * Time.deltaTime;
+	private void ProcessRotation()
+	{
+		float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+		float pitchDueToThrow = yThrow * rotationPitchFactor;
+		float pitch = pitchDueToPosition + pitchDueToThrow;
+		float yaw = xThrow * rotationYawFactor;
+		float roll = xThrow * rotationRollFactor;
+		transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+	}
 
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
-    }
+	private void ProcessTranslation()
+	{
+		xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+		float xOffset = xRange * xThrow * Time.deltaTime;
+		float xRaw = transform.localPosition.x + xOffset;
+		float clampedXPos = Mathf.Clamp(xRaw, -xRange, xRange);
+
+		float yOffset = yRange * yThrow * Time.deltaTime;
+		float yRaw = transform.localPosition.y + yOffset;
+		float clampedYPos = Mathf.Clamp(yRaw, -yRange, yRange);
+
+		transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+	}
 }
